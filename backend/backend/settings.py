cf4ac4ad72f1cef046badbac112535c120c4199f
Ayconey <Ayconey@gmail.com>
2022-09-19 14:25:46 +0200

    connected django to postgres

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("DB_NAME",'db'),
+        'USER': os.environ.get("DB_USER",'user'),
+        'PASSWORD': os.environ.get("DB_PASS",'password'),
+        'HOST':os.environ.get("DB_HOST",'db'),
+        'PORT':'5432',
     }
 }
 
